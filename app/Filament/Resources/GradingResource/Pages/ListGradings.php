5160f171dcc655c7d6c68208cdfbe7602dc3d3a3
Ferry Ardiansyah <ferard.diansyah@gmail.com>
2026-07-08 10:07:39 +0700

    feat: make ListGradings, ListInvestigasiSederhanas, and ListRootCauseAnalyses pages full-width

--- app/Filament/Resources/GradingResource/Pages/ListGradings.php
+++ app/Filament/Resources/GradingResource/Pages/ListGradings.php
@@ -16,4 +16,9 @@ class ListGradings extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    public function getMaxContentWidth(): string
+    {
+        return 'full';
+    }
 }
